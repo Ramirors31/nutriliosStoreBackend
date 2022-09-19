ebbcb358d0ed90b110b3116df9d13e4adce18d18
RamiroRS <ramirezsiqueiros@hotmail.com>
2022-09-19 13:25:44 -0500

    Se inicializó el servidor correctamente y se realizó la conexión local a la base de datos sql

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const mysql = require('mysql');
+const cors = require('cors');
 
 
 //SETTINGS
@@ -10,6 +11,7 @@ app.set('port', 3000);
 //MIDDLEWARES
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({extended: false}));
 
 
@@ -25,22 +27,22 @@ connection.connect((err) => {
     console.log("Connected to database");
 })
 
-const readQuery = "SELECT * FROM `productos`"
-
-const readData = connection.query(readQuery, (error, results, fields) => {
-    if (error) {
-        console.error(error);
-        return;
-    } else {
-        console.log(results);
-    }
-
-});
-console.log(readData);
 
 //ROUTES
 app.get('/', (req, res) => {
     res.send('Hello world');
+});
+
+app.get('/inventaryData', (req, res) => {
+    const readQuery = "SELECT * FROM `products`"
+    connection.query(readQuery, (error, results, fields) => {
+        if (error) {
+            console.error(error);
+            return;
+        } else {
+            res.send(results);
+        }
+    });
 });
 
 
