529b8a70289d948601616707cd14dd88a4f18c60
RamiroRS <ramirezsiqueiros@hotmail.com>
2022-09-23 14:41:12 -0500

    base de servidos, algunas funciones generales para añadir y buscar registros

--- src/index.js
+++ src/index.js
@@ -27,24 +27,85 @@ connection.connect((err) => {
     console.log("Connected to database");
 })
 
-
 //ROUTES
 app.get('/', (req, res) => {
     res.send('Hello world');
 });
 
-app.get('/inventaryData', (req, res) => {
-    const readQuery = "SELECT * FROM `products`"
+app.post('/getAllTableData', (req, res) => {
+    const {tableName} = req.body;
+    const readQuery = `SELECT * FROM ${tableName}`;
     connection.query(readQuery, (error, results, fields) => {
         if (error) {
             console.error(error);
+            res.end();
             return;
-        } else {
+        } else if (results) {
             res.send(results);
         }
     });
 });
 
+app.post('/insertIntoDB', (req,res) => {
+    const {tableName, tableColumns, insertValues} = req.body;
+    const numOfColumns = tableColumns.length;
+    const numOfValues = insertValues.length;
+    let query = `INSERT INTO ${tableName} (`;
+    tableColumns.forEach((columnName, index) => {
+        query += `${columnName}`;
+        if(index + 1 === numOfColumns) {
+            query += ') '; 
+        } else {
+            query += ', ';
+        }
+    });
+    query += `VALUES (`;
+    insertValues.forEach((value, index) => {
+        query +=`'${value}'`
+        if (index + 1 === numOfValues) {
+            query += ') ';
+        } else {
+            query += ', ';
+        }
+    });
+    connection.query(query, (error, results, fields) => {
+        if (error) {
+            console.error(error);
+            res.end();
+            return;
+        } else if (results) {
+            res.send('200');
+            console.log('Insertado con insertIntoDB todo bn todo bn');
+            console.log(results);
+        }
+    });
+});
+
+app.post('/updateTableRecord', (req, res) => {
+    const {tableName, tableColumns, updateValues, recordID, identifier} = req.body;
+    const columnsLength = tableColumns.length;
+    let updateQuery = `UPDATE ${tableName} SET `;
+    tableColumns.forEach((columnName, index) => {
+        if (index + 1 < columnsLength) {
+            updateQuery += `'${columnName}' = ?, `
+        } else {
+            updateQuery += `'${columnName}' = ? `
+        }
+    });
+    updateQuery += `WHERE ${identifier} = ${recordID}`;
+    console.log(updateQuery);
+    connection.query(updateQuery, updateValues, (error, results, rows, fields) => {
+        if (error) {
+            console.error(error);
+            res.end();
+            return;
+        } else if (results) {
+            console.log('Updateado todo bnn')
+            console.log(results);
+            res.send();
+        }
+    })
+});
 
 //INITIALIZING SERVER
 app.listen(app.get('port'), () => {
